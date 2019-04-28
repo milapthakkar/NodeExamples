55e52806646b607cc27bdfff2e2dbef7e97ee766
milap thakkar <milap.thakkar1987@gmail.com>
2019-04-29 02:01:44 +0530

    JWT Demo
    JWT Demo

---
+++ 07-JWT/config.js
@@ -0,0 +1,6 @@
+﻿module.exports={
+    port : 3000,
+    hostname : "localhost",
+    connectionString: "mongodb://sa:admin123@ds151433.mlab.com:51433/demoapp",
+    secret: "AnyStringCanBeYourSecretKey"
+}
---
+++ 07-JWT/server.js
@@ -0,0 +1,45 @@
+﻿require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('helpers/jwt');
+const errorHandler = require('helpers/error-handler');
+const config = require('config.js');
+const mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// use JWT auth to secure the api
+app.use(jwt());
+
+// api routes
+app.use('/api/users', require('./users/users.controller'));
+
+// global error handler
+app.use(errorHandler);
+
+// Connecting to the database
+mongoose.connect(config.connectionString, {   
+	useNewUrlParser: true
+}).then(() => {
+    console.log("Database connected successfully");    
+}).catch(err => {
+    console.log('Database connection failed', err);
+    process.exit();
+});
+
+// define a simple route 
+app.get('/', (req, res) => {
+    res.status(200).send("Api Is Working.");
+});
+
+// start server
+const port = process.env.PORT || config.port;
+const hostname = config.hostname;
+// listen for requests
+app.listen(port, () => {
+    console.log(`Server is listening on port http://${hostname}:${port}`);
+}); 
